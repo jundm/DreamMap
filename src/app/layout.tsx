adf7f5016b4a7ce4bf8ba42f659d8d7d4b28b86d
devJun <>
2024-07-29 16:09:28 +0900

    feat: 새 페이지 컴포넌트 추가
    디테일 페이지와 새로운 매토리 페이지 컴포넌트를 추가했습니다. 각각의 컴포넌트는 기본적인 JSX 구조를 가지고 있으며, 매토리 페이지는 클라이언트 사이드 렌더링을 위해 'use client'를 사용합니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
-import Sidebar from "@component/Sidebar";
+import Sidebar from "@component/sidebar/Sidebar";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -18,7 +18,7 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={inter.className}>
-        <div className={'sidebar'}>
+        <div className='sidebar'>
         <Sidebar/>
         {children}
         </div>
